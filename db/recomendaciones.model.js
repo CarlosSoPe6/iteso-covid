72d351372bb17cacdbabba01075306bf686b9e88
Carlos Soto Pérez <carlos348@outlook.com>
2020-10-31 16:37:19 -0600

    Cambio de nombre de tabla

--- db/recomendaciones.model.js
+++ db/recomendaciones.model.js
@@ -4,7 +4,7 @@ async function getAll() {
   const connection = await getConnection();
 
   return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM recomendaciones', (err, results) => {
+    connection.query('SELECT * FROM Recomendaciones', (err, results) => {
       if (err) {
         reject(err);
         return;
@@ -18,7 +18,7 @@ async function postNew(body) {
   const connection = await getConnection();
 
   return new Promise((resolve, reject) => {
-    const q = `INSERT INTO recomendaciones (nivelRecomendaciones, descripcion) VALUES (${body.nivel}, '${body.descripcion}')`;
+    const q = `INSERT INTO Recomendaciones (nivelRecomendaciones, descripcion) VALUES (${body.nivel}, '${body.descripcion}')`;
 
     connection.query(q, (err, results) => {
       if (err) {
@@ -34,7 +34,7 @@ async function getByID(id) {
   const connection = await getConnection();
 
   return new Promise((resolve, reject) => {
-    connection.query(`SELECT * FROM recomendaciones WHERE IdRecomendaciones = ${id}`, (err, results) => {
+    connection.query(`SELECT * FROM Recomendaciones WHERE IdRecomendaciones = ${id}`, (err, results) => {
       if (err) {
         reject(err);
         return;
@@ -48,7 +48,7 @@ async function putByID(id, body) {
   const connection = await getConnection();
 
   return new Promise((resolve, reject) => {
-    connection.query(`UPDATE recomendaciones SET nivelRecomendaciones = ${body.nivel}, descripcion = '${body.descripcion}' WHERE IdRecomendaciones = ${id}`, (err, results) => {
+    connection.query(`UPDATE Recomendaciones SET nivelRecomendaciones = ${body.nivel}, descripcion = '${body.descripcion}' WHERE IdRecomendaciones = ${id}`, (err, results) => {
       if (err) {
         reject(err);
         return;
@@ -62,7 +62,7 @@ async function deleteByID(id) {
   const connection = await getConnection();
 
   return new Promise((resolve, reject) => {
-    connection.query(`DELETE FROM recomendaciones WHERE IdRecomendaciones = ${id}`, (err, results) => {
+    connection.query(`DELETE FROM Recomendaciones WHERE IdRecomendaciones = ${id}`, (err, results) => {
       if (err) {
         reject(err);
         return;
@@ -76,7 +76,7 @@ async function getByNivel(nivel) {
   const connection = await getConnection();
 
   return new Promise((resolve, reject) => {
-    connection.query(`SELECT * FROM recomendaciones WHERE nivelRecomendaciones = ${nivel}`, (err, results) => {
+    connection.query(`SELECT * FROM Recomendaciones WHERE nivelRecomendaciones = ${nivel}`, (err, results) => {
       if (err) {
         reject(err);
         return;
